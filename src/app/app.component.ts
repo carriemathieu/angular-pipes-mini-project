d4bf97e2990b058207af572cd2b77dcc0c59598b
Carrie Mathieu <carrieburks@gmail.com>
2021-08-05 09:07:51 -0400

    add html template, add logic in appcomponent.ts to handle displaying input value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pipes-mini-project';
+  // ! = "definite assignment assertion", name = string | undefined. "There is a property called name with a type of string | undefined. It starts with a value of undefined. But every time I get or set that property, I want to treat it as type string."
+
+  name!: string;
+
+  onNameChange(value: string) {
+    this.name = value
+  }
 }
