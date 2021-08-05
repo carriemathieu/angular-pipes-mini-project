f07206aeb859d0a0e4d629e78651e6d37c740928
Carrie Mathieu <carrieburks@gmail.com>
2021-08-05 10:10:24 -0400

    add date & amount input fields and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  // ! = "definite assignment assertion", name = string | undefined. "There is a property called name with a type of string | undefined. It starts with a value of undefined. But every time I get or set that property, I want to treat it as type string."
 
+export class AppComponent {
+
+  // ! = "definite assignment assertion", name = string | undefined. "There is a property called name with a type of string | undefined. It starts with a value of undefined. But every time I get or set that property, I want to treat it as type string."
   name!: string;
+  date!: string;
+  amount!: number;
 
   onNameChange(value: string) {
-    this.name = value
+    this.name = value;
   }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  // value is string because we are receiving input value, which is always string
+  onAmountChange(value: string){
+    this.amount = parseFloat(value)
+    console.log(this.amount)
+  }
+
 }
